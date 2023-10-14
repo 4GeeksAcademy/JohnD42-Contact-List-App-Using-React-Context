b0c722104ce4d110d9b236d436809e63ac3c3c0e
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-10-14 00:27:23 +0000

    returned to useState architecture for form page to avoid problems when users change just one field

--- src/js/views/UpdateContact.jsx
+++ src/js/views/UpdateContact.jsx
@@ -8,6 +8,22 @@ export const UpdateContact = () => {
     const navigate = useNavigate();
     const { idx } = useParams();
 
+    if(idx === undefined) {
+        const contact = {
+            full_name: '',
+            phone: '',
+            email: '',
+            address: '',
+        }
+    } else {
+        const contact = store.contacts[idx]
+    }
+    const contact = store.contacts[idx]
+    const [fullName, setFullName] = useState(contact.full_name)
+    const [phone, setPhone] = useState(contact.phone)
+    const [email, setEmail] = useState(contact.email)
+    const [address, setAddress] = useState(contact.address)
+
     const submitHandler = async e => {
         e.preventDefault();
         await actions.formSubmitHandler(e, idx);
@@ -22,19 +38,19 @@ export const UpdateContact = () => {
                 </div>
                 <label>Full Name</label>
                 <div className="input-group col-12 my-3">
-                    <input name="full_name" type="text" className="form-control" placeholder={idx === undefined ? "Full Name" : store.contacts[idx].full_name} aria-label="Full Name" aria-describedby="basic-addon2"/>
+                    <input name="full_name" type="text" className="form-control" placeholder={idx === undefined ? "Full Name" : store.contacts[idx].full_name} value={fullName} onChange={ev => setFullName(ev.target.value)} aria-label="Full Name" aria-describedby="basic-addon2"/>
                 </div>
                 <label>Email</label>
                 <div className="input-group col-12 my-3">
-                    <input name="email" type="text" className="form-control" placeholder={idx === undefined ? "Enter Email" : store.contacts[idx].email} aria-label="Enter Email" aria-describedby="basic-addon2"/>
+                    <input name="email" type="text" className="form-control" placeholder={idx === undefined ? "Enter Email" : store.contacts[idx].email} value={email} onChange={ev => setEmail(ev.target.value)} aria-label="Enter Email" aria-describedby="basic-addon2"/>
                 </div>
                 <label>Phone</label>
                 <div className="input-group col-12 my-3">
-                    <input name="phone" type="text" className="form-control" placeholder={idx === undefined ? "Enter Phone" : store.contacts[idx].phone} aria-label="Enter Phone" aria-describedby="basic-addon2"/>
+                    <input name="phone" type="text" className="form-control" placeholder={idx === undefined ? "Enter Phone" : store.contacts[idx].phone} value={phone} onChange={ev => setPhone(ev.target.value)} aria-label="Enter Phone" aria-describedby="basic-addon2"/>
                 </div>
                 <label>Email</label>
                 <div className="input-group col-12 my-3">
-                    <input name="address" type="text" className="form-control" placeholder={idx === undefined ? "Enter Address" : store.contacts[idx].address} aria-label="Enter Address" aria-describedby="basic-addon2"/>
+                    <input name="address" type="text" className="form-control" placeholder={idx === undefined ? "Enter Address" : store.contacts[idx].address} value={address} onChange={ev => setAddress(ev.target.value)} aria-label="Enter Address" aria-describedby="basic-addon2"/>
                 </div>
                 <div className="col-12">
                     <button className="btn btn-primary w-100 h-100" type='submit'>Save</button>
